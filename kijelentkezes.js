963192807218a0a9ffa5a29099758a0b216b173c
Paleksz <alexka0813@gmail.com>
2024-12-04 18:20:21 +0100

    Admin felülettel kiegészítés

---
+++ kijelentkezes.js
@@ -0,0 +1,14 @@
+// Kijelentkezés funkció
+document.getElementById("logout-button").addEventListener("click", function () {
+    window.location.href = "index.html";
+});
+
+// Navigáció a Sofőrök oldalra
+document.getElementById("drivers-button").addEventListener("click", function () {
+    window.location.href = "drivers.html";
+});
+
+// Navigáció az Útvonalak oldalra
+document.getElementById("routes-button").addEventListener("click", function () {
+    window.location.href = "routes.html";
+});
